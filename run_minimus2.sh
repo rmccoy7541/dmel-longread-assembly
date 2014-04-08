23305f1fe947da23d4479e16b77ecc9473f651c9
Rajiv McCoy <rmccoy@stanford.edu>
2014-04-08 16:36:41 -0700

    add minimus script

---
+++ run_minimus2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# convert celera assembly to Amos format
+toAmos -s celera_output.fa -o minimus_input
+
+# run minimus2 on celera assembly
+minimus2 minimus_input -D REFCOUNT=0 -D MINID=99.9 -D OVERLAP=800 -D MAXTRIM=1000 -D WIGGLE=15 -D CONSERR=0.01
+
+# concatenate supercontigs and singletons (i.e. celera contigs)
+cat minimus_input.fasta minimus_input.singletons.seq > minimus_output.fa
